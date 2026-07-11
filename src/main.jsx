0e4428fc778511546d1cde990f4fa72df2d431a1
lovakov <lovakov@gmail.com>
2026-07-11 12:31:44 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import Yearle from './Yearle.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Yearle />
-  </React.StrictMode>
-)
+createRoot(document.getElementById('root')).render(<Yearle />)
